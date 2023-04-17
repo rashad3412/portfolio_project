1ddd4332bc8eca77c9d3801f0d2ccb6d931820ef
rashad3412 <rashd3412@gmail.com>
2023-04-17 15:54:00 -0400

    updated files

--- javascript.js
+++ javascript.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 const form = document.querySelector("#form")
    const submitButton = document.querySelector("#submit")
    const scriptURL = "https://httpbin.org/post";
-=======
-// alert('Hello, World!');
-   const form = document.querySelector("#form")
-   const submitButton = document.querySelector("#submit")
-   const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
->>>>>>> 5cd6adb1498900aaebe8837da85f2699284d0df1
 
    form.addEventListener('submit', e => {
      submitButton.disabled = true
@@ -16,11 +9,7 @@ const form = document.querySelector("#form")
      fetch(scriptURL, { method: 'POST', body: requestBody})
        .then(response => {
           alert('Success!', response)
-<<<<<<< HEAD
           submitButton.disabled = ture
-=======
-          submitButton.disabled = false
->>>>>>> 5cd6adb1498900aaebe8837da85f2699284d0df1
          })
        .catch(error => {
        alert('Error!', error.message)
@@ -28,8 +17,4 @@ const form = document.querySelector("#form")
 
        }
        )
-<<<<<<< HEAD
    })
-=======
-   })
->>>>>>> 5cd6adb1498900aaebe8837da85f2699284d0df1
